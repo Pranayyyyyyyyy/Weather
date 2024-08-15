9006c733ce2c4a5ce109056f05f66ca3525a18f8
Pranay <pranaynair05@gmail.com>
2024-08-15 14:39:30 +0530

    Updated weather display and fixed bug in temperature calculation

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-
+const weatherIcon=document.querySelector(".weather-icon");
 async function checkWeather(city) {
     const response = await fetch(url + city + `&appid=${api}`);
     var data = await response.json();
-    console.log(data);
+
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    if(data.checkWeather[0].main=="Clouds"){
+        weatherIcon.src="images/clouds.png";
+        
+    }
+    else  if(data.checkWeather[0].main=="Clear"){
+        weatherIcon.src="images/clear.png";
 }
-
+else  if(data.checkWeather[0].main=="Rain"){
+    weatherIcon.src="images/rain.png";
+}
+else  if(data.checkWeather[0].main=="Drizzle"){
+    weatherIcon.src="images/drizzle.png";
+}
+else  if(data.checkWeather[0].main=="Mist"){
+    weatherIcon.src="images/mist.png";
+}
+document.querySelector(".weather").style.display="block";
+}
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 });
